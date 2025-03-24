492d34513a506ffabc5dff8f9808f94507da7a1f
louisepaul75 <ricafortlouisepaul75@gmail.com>
2025-03-25 07:56:44 +0800

    sizes api modified

--- app/api/sizes/route.ts
+++ app/api/sizes/route.ts
@@ -20,7 +20,6 @@ export async function POST(req: Request) {
         });
       })
     );
-
     const categorySizeCreateManyInput = createdSizes.map((size) => ({
       categoryId,
       sizeId: size.id,
